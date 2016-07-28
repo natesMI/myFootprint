34109416cfb3181370b30914251711769d153af3
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-07-28 09:34:00 -0700

    removes material design for android and install cross platform material kit

--- app/containers/footPrint.js
+++ app/containers/footPrint.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Actions from 'react-native-router-flux';
-import { Icon } from 'react-native-material-design';
 
 import {
 	View,
