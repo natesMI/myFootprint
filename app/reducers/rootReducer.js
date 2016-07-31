601191e65f8478d844d6e55d435d0f152e549fe4
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-07-31 15:33:40 -0700

    Updates reducer with logout logic

--- app/reducers/rootReducer.js
+++ app/reducers/rootReducer.js
@@ -2,9 +2,26 @@ import { combineReducers } from 'redux';
 import landingReducer from './landingReducer';
 import footprintReducer from './footprintReducer';
 
+/*
 const rootReducer = combineReducers({
 	landing: landingReducer,
 	footprint: footprintReducer
 });
+
+export default rootReducer;
+*/
+
+const appReducer = combineReducers({
+  landing: landingReducer,
+  footprint: footprintReducer
+})
+
+const rootReducer = (state, action) => {
+  if (action.type === 'USER_LOGOUT') {
+    state = undefined
+  }
+
+  return appReducer(state, action)
+}
 
 export default rootReducer;
