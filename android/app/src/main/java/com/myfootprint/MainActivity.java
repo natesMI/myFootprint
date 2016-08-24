4a7202dda035ab34581861561b22ce063e43706f
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-08-24 14:16:15 -0700

    Revert "adds fonts and configures react native maps"
    This reverts commit 4e497722aa111297ab7700710b875d540b59857f.

--- android/app/src/main/java/com/myfootprint/MainActivity.java
+++ android/app/src/main/java/com/myfootprint/MainActivity.java
@@ -1,9 +1,6 @@
 package com.myfootprint;
 
 import com.facebook.react.ReactActivity;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.github.xinthink.rnmk.ReactMaterialKitPackage;
-import com.airbnb.android.react.maps.MapsPackage;
 
 public class MainActivity extends ReactActivity {
 
--- app/containers/footPrint.js
+++ app/containers/footPrint.js
@@ -2,43 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Actions from 'react-native-router-flux';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import MapView from 'react-native-maps';
 
 import {
 	View,
 	Text,
-	StyleSheet
+	StyleSheet,
+	Navigator,
+	TouchableHighlight,
+	TabBarIOS,
+  MapView
 } from 'react-native';
 
-/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   }
-});
-
-*/
-const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
 });
 
 class FootPrint extends React.Component{
@@ -52,21 +32,14 @@ class FootPrint extends React.Component{
 
 	render(){
 		return (
-            <View style ={styles.container}>
-                <MapView
-                    style={styles.map}
-                    region={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.015,
-                        longitudeDelta: 0.0121,
-                    }}
-                    >
-                </MapView>
-            </View>
-        )
-    }
-
+	  	  <MapView
+            style={{flex: 1}}
+            showsUserLocation={true}
+            followUserLocation={true}
+            region={{latitude: 37.774088, longitude:  -122.437642, latitudeDelta: .2, longitudeDelta: .2}}
+          />
+			);
+	}
 }
 
 const mapStateToProps = state => ({
@@ -76,6 +49,3 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(FootPrint);
-
-/*region={{latitude: 37.774088, longitude:  -122.437642, latitudeDelta: .2, longitudeDelta: .2}}*/
-
